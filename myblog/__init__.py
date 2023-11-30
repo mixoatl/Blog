91fa90a5d54134c2031b38a1f9ac52761a61568e
HectorCamachoC <92535536+HectorCamachoC@users.noreply.github.com>
2023-11-29 20:06:53 -0600

    Update __init__.py

--- myblog/__init__.py
+++ myblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
 
 app = Flask(__name__)
 
@@ -9,7 +10,9 @@ db = SQLAlchemy(app)
 
 #Importar vistas 
 from myblog.views.auth import auth
+from myblog.views.admin import admin as admin_blueprint
 app.register_blueprint(auth)
+app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
 from myblog.views.blog import blog
 app.register_blueprint(blog)
