c13449d0ed45fef0129ecd211937befc065761af
HectorCamachoC <92535536+HectorCamachoC@users.noreply.github.com>
2023-11-29 20:28:40 -0600

    Update user.py

--- myblog/models/user.py
+++ myblog/models/user.py
@@ -5,10 +5,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
     password = db.Column(db.Text)
+    is_admin = db.Column(db.Boolean, default=False)
 
     def __init__(self, username, password) -> None:
         self.username = username
         self.password = password
+        self.is_admin = is_admin
 
     def __repr__(self) -> str:
         return f'User: {self.username}'
